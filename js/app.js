3569edac366ca9e1f182f847ba92d6fc0f54d090
MalwinaGruchot <malwinagruchot@wp.pl>
2023-06-22 23:50:40 +0200

    add js-objects in object and html-change

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const form = document.querySelector(".calc__form");
 const inputList = document.querySelectorAll(".calc__input");
 const ulChoiceList = document.querySelector(".calc__list");
 const container = document.querySelector(".calc__wrap");
-const arrayObjects = [];
+const btnSpan = document.querySelector(".calc__priceTotal--bold");
 let totalPrice = 0;
 
 ////////////////////
@@ -27,98 +27,99 @@ inputList[2].addEventListener("click", addClassActive);
 
 ////////////////////////////////////////////
 const Input = function () {
-    this.value = 0;
-    this.price = 0;
-    this.id = "";
-    this.totalPrice = 0;
-    this.package = "";
-    this.counter = false;
+}
+
+Input.prototype.addInput = function (id) {
+    this[id] = {
+        value: 0,
+        price: 0,
+        totalPrice: 0,
+        package: ""
+    }
 }
 
 Input.prototype.getValueInput = function (e) {
-    this.value = 1 * e.target.value;
-    this.price = 1 * e.target.getAttribute("data-price")
-    this.id = e.target.id;
-    this.totalPrice = this.value * this.price;
+    this[e.target.id]["value"] = (e.target.type === "checkbox") ? 1 * e.target.checked : 1 * e.target.value;
+    this[e.target.id]["price"] = 1 * e.target.getAttribute("data-price")
+    this[e.target.id]["totalPrice"] = this[e.target.id]["value"] * this[e.target.id]["price"];
 };
-Input.prototype.getValueCheckbox = function (e) {
-    this.value = 1 * e.target.checked;
-    this.price = 1 * e.target.getAttribute("data-price")
-    this.id = e.target.id;
-    this.totalPrice = this.value * this.price;
-}
+
 
 Input.prototype.getValueSelect = function (e) {
     switch (e.target.innerText) {
         case "Basic":
-            this.value = 1
-            this.price = 40
-            this.package = "Basic"
+            this[e.currentTarget.id]["value"] = 1
+            this[e.currentTarget.id]["price"] = 40
+            this[e.currentTarget.id]["package"] = "Basic"
             break;
         case "Professional":
-            this.value = 1;
-            this.price = 50;
-            this.package = "Professional"
+            this[e.currentTarget.id]["value"] = 1;
+            this[e.currentTarget.id]["price"] = 50;
+            this[e.currentTarget.id]["package"] = "Professional"
             break;
         case "Premium":
-            this.value = 1;
-            this.price = 60
-            this.package = "Premium"
+            this[e.currentTarget.id]["value"] = 1;
+            this[e.currentTarget.id]["price"] = 60
+            this[e.currentTarget.id]["package"] = "Premium"
             break;
         default:
-            this.value = 0;
+            this[e.currentTarget.id]["value"] = 0;
     }
-    this.id = e.currentTarget.id;
-    this.totalPrice = this.value * this.price;
+    this[e.currentTarget.id]["totalPrice"] = this[e.currentTarget.id]["value"] * this[e.currentTarget.id]["price"];
 
 }
 
 
-Input.prototype.displayElement = function (inputObject) {
-    const div = document.querySelector(`.${inputObject.id}`)
-    if (inputObject.value) {
+Input.prototype.displayElement = function (e) {
+    const div = document.querySelector(`.${e.currentTarget.id}`)
+
+    if (this[e.currentTarget.id]["value"]) {
         div.classList.add("displayFlex")
         if (div.querySelector(".calc__calculation")) {
-            div.querySelector(".calc__calculation").innerText = `${this.value}*$${this.price}`
+            div.querySelector(".calc__calculation").innerText = `${this[e.currentTarget.id]["value"]}*$${this[e.target.id]["price"]}`
         };
         if (div.querySelector(".calc__package")) {
-            div.querySelector(".calc__package").innerText = this.package;
+            div.querySelector(".calc__package").innerText = this[e.currentTarget.id]["package"];
         }
-        div.querySelector(".calc__price").innerText = `$${this.totalPrice}`;
+        div.querySelector(".calc__price").innerText = `$${this[e.currentTarget.id]["totalPrice"]}`;
     } else {
         div.classList.remove("displayFlex")
     }
 
 }
 
+Input.prototype.getTotalPrice = function () {
+    totalPrice = 0;
+    const array = Object.values(this);
+    console.log(array)
+    array.forEach(function (item) {
+        totalPrice = totalPrice + item.totalPrice;
+    })
+    btnSpan.innerText = `$${totalPrice}`
+}
 
 const handleDisplayFieldInput = function (e) {
-    const inputObject = new Input();
-    inputObject.getValueInput(e);
-    console.log(inputObject);
-    inputObject.displayElement(inputObject);
-    arrayObjects.push(inputObject)
+    inputs.addInput(e.target.id)
+    inputs.getValueInput(e);
+    inputs.displayElement(e);
+    inputs.getTotalPrice();
 
 }
-
-const handleDisplayFieldCheckbox = function (e) {
-    const inputObject = new Input();
-    inputObject.getValueCheckbox(e);
-    console.log(inputObject);
-    inputObject.displayElement(inputObject);
-}
-
 const handleDisplayFieldSelect = function (e) {
-    const inputObject = new Input();
-    inputObject.getValueSelect(e);
-    console.log(inputObject);
-    inputObject.displayElement(inputObject);
+    inputs.addInput(e.currentTarget.id)
+    inputs.getValueSelect(e);
+    console.log(inputs);
+    inputs.displayElement(e);
+    inputs.getTotalPrice();
+
 }
 
-
+const inputs = new Input();
+console.log(inputs)
 inputList[0].addEventListener("input", handleDisplayFieldInput)
 inputList[1].addEventListener("input", handleDisplayFieldInput)
 ulChoiceList.addEventListener("click", handleDisplayFieldSelect)
-inputList[3].addEventListener("change", handleDisplayFieldCheckbox)
-inputList[4].addEventListener("change", handleDisplayFieldCheckbox)
+inputList[3].addEventListener("change", handleDisplayFieldInput)
+inputList[4].addEventListener("change", handleDisplayFieldInput)
+
 
